225301fef8f75e67ed77c9649a9cbafeb6c6562d
Touheed Khan <touheedkhan93@gmail.com>
2021-03-30 12:46:33 +0530

    api version and validation added

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -17,10 +17,17 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version private Long version;
+
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
+
     @Column(length=1000000)
     @Lob
     private String body;
+
     @Temporal(TemporalType.DATE)
     @CreationTimestamp
     private Date date;
